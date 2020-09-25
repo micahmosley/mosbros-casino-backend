0bcc15f70a156b974478bbc21b747d52e4f3daf2
cam4994 <colin.a.mosley@gmail.com>
2020-09-25 16:09:34 -0500

    Changed database

--- db/schema.rb
+++ db/schema.rb
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2020_09_25_194720) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.integer "bank"
     t.integer "game_id"
     t.datetime "created_at", precision: 6, null: false
